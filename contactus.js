c2ed0bac980ea1535229db1ce750e3da7370a24b
Jetë Lajçi <74615773+Jeta52@users.noreply.github.com>
2022-01-14 22:26:12 +0100

    Add files via upload

--- contactus.js
+++ contactus.js
@@ -1,13 +1,4 @@
-for (var i=0; i<=300000000; i+=1){
-  var j=i;
- }
- postMessage(j);
-
-
-
-
 var currentdate = new Date(); 
-
 
 var datetime =  currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
@@ -17,3 +8,41 @@ var datetime =  currentdate.getDate() + "/"
                 + currentdate.getSeconds();
 
 document.getElementById("koha").innerHTML = datetime;
+
+$(document).ready(function(){
+
+    // Add new element
+    $(".add").click(function(){
+
+        // Finding total number of elements added
+        var total_element = $(".element").length;
+                    
+        // last <div> with element class id
+        var lastid = $(".element:last").attr("id");
+        var split_id = lastid.split("_");
+        var nextindex = Number(split_id[1]) + 1;
+
+        var max = 5;
+        // Check total number elements
+        if(total_element < max ){
+            // Adding new div container after last occurance of element class
+            $(".element:last").after("<div class='element' id='div_"+ nextindex +"'></div>");
+                        
+            // Adding element to <div>
+            $("#div_" + nextindex).append("<input type='text' placeholder='Enter your question' id='txt_"+ nextindex +"'>&nbsp;<span id='remove_" + nextindex + "' class='remove'>&#10005;</span>");
+                    
+        }
+                    
+    });
+
+    // Remove element
+    $('.container').on('click','.remove',function(){
+                
+        var id = this.id;
+        var split_id = id.split("_");
+        var deleteindex = split_id[1];
+
+        // Remove <div> with id
+        $("#div_" + deleteindex).remove();
+    });                
+});
